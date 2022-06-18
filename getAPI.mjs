9c0461e34636870ade56535dcfda43417befc217
maashi <octtn8c@gmail.com>
2022-06-18 16:52:28 +0900

    fix: 外部API情報をmicroCMSに登録する

--- getAPI.mjs
+++ getAPI.mjs
@@ -1,7 +1,55 @@
 import fetch from 'node-fetch'
 
-console.log('API test')
 const response = await fetch('https://pokeapi.co/api/v2/pokemon/')
-const body = await response.json()
+const responseData = await response.json()
+const max = await responseData.results.length
+let count = 0
 
-console.log(body)
+const loop = setInterval(() => {
+  if (count >= max - 1) {
+    clearInterval(loop)
+  }
+
+  const pokeData = responseData.results[count]
+
+  const deletePost = (id) => {
+    if (id) {
+      fetch(`https://external-api.microcms.io/api/v1/poke/${id}`, {
+        method: 'DELETE',
+        headers: {
+          'X-MICROCMS-API-KEY': '9fa2e135ed5c4d6e90794ada6972c939675f',
+        },
+      })
+    }
+  }
+
+  fetch(
+    `https://external-api.microcms.io/api/v1/poke/?filters=name[equals]${pokeData.name}`,
+    {
+      method: 'GET',
+      headers: {
+        'X-MICROCMS-API-KEY': '9fa2e135ed5c4d6e90794ada6972c939675f',
+      },
+    }
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.totalCount) {
+        deletePost(data.contents[0].id)
+      }
+    })
+
+  fetch(`https://external-api.microcms.io/api/v1/poke/`, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      'X-MICROCMS-API-KEY': '9fa2e135ed5c4d6e90794ada6972c939675f',
+    },
+    body: JSON.stringify({
+      name: pokeData.name,
+      url: pokeData.url,
+    }),
+  })
+
+  count++
+}, 1000)
